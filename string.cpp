c7acb2ee3c6a765c8916b3c6549d309c9a20be77
Thaiany <thaianyemilydonascimentomosque@gmail.com>
2026-01-20 20:38:25 +0000

    texto

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s1 = "Hello";
+    std::string s2 = "Hello again!";
+
+    std::cout << s1 << std::endl;
+    std::cout << s2 << std::endl;
+
+    return 0;
+}
