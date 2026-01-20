e37471483a5bf8613a14d09c98c1ccb0908ec2f7
Thaiany <thaianyemilydonascimentomosque@gmail.com>
2026-01-20 15:00:36 +0000

    float e Double

---
+++ floatEdouble.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    float F = 3.14159265359;
+    double D = 3.14159265359;
+    float intemprice = 24.99F;
+    double temporature = 23.5;
+
+    std::cout << "F : " << F << std::endl;
+    std::cout << "D : " << D << std::endl;
+    std::cout << "Price : " << intemprice << std::endl;
+    std::cout << "Temperature : " << temporature << std::endl;
+    return 0;
+}
