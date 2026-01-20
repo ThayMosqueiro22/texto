2e74a813f85649cc782620f09b9a3c91c0cb95eb
Thaiany <thaianyemilydonascimentomosque@gmail.com>
2026-01-20 13:38:48 +0000

    valor

---
+++ valor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int score = 100;
+    int bonus = 25;
+
+    std::cout << "Score : " << score << std::endl;
+    std::cout << "Bonus : " << bonus << std::endl;
+    
+    return 0;
+}
